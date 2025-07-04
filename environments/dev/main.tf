62e7d4f2b5a11c604c3c8af9af407d3ef5b258b9
Hridayananda <barmanhriday@gmail.com>
2025-07-04 12:50:33 +0530

    intended changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source        = "../../module/ec2"
+  source        = "../../module/ec2" #this is the path to ec2 which will be changed in every enviroment
   instance_type = var.instance_type
   subnet_id     = module.vpc.subnet_id
   vpc_id        = module.vpc.vpc_id
